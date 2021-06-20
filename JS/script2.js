f3baf1024603db4fd7eb934fba5a29c47afa90b0
beavis <asd@gmail.com>
2021-06-20 22:01:47 +0300

    first commit

---
+++ JS/script2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert('hi');
+
+// const res = confirm("Are you fucking all right?");
+// console.log(res);
+
+// const answer = +prompt("are you gay?", "12");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Имя?', '');
+answers[1] = prompt('фамилия?', '');
+answers[2] = prompt('Лох?', '');
+
+
+console.log(typeof(answers));
+console.log(typeof(null));
